d422b0a418820ddf28027e1d7816547e553fc7b2
strmbld <strmbld@gmail.com>
2021-10-06 13:25:17 +0300

    Made entities

---
+++ src/Entity/Figure.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FigureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FigureRepository::class)
+ */
+class Figure
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Color::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $color;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Shape::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $shape;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getColor(): ?Color
+    {
+        return $this->color;
+    }
+
+    public function setColor(?Color $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getShape(): ?Shape
+    {
+        return $this->shape;
+    }
+
+    public function setShape(?Shape $shape): self
+    {
+        $this->shape = $shape;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Record.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecordRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RecordRepository::class)
+ */
+class Record
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
